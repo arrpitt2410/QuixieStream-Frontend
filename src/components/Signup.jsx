f517c976367f2aaa8440a4b75e71bd2893a0df55
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-09-12 12:27:50 +0530

    initial-committtt

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Context } from '../main';
+import toast from 'react-hot-toast';
 
 const Signup = () => {
   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
